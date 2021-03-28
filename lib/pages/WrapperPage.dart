0e6bf83bdc08674b77012c6cb7662dd1e8d3841b
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2021-03-28 18:24:58 +0200

    Working on the UI

--- lib/pages/WrapperPage.dart
+++ lib/pages/WrapperPage.dart
@@ -9,24 +9,36 @@ class _WrapperPageState extends State<WrapperPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[900],
       body: Stack(
         children: [
-          Column(
+          Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 40,
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [],
-                  ),
-                ),
-              ),
-              Row(
+              // Padding(
+              //   padding: const EdgeInsets.only(right: 8.0),
+              //   child: Container(
+              //     width: 100,
+              //     height: MediaQuery.of(context).size.height,
+              //     color: Colors.grey[300],
+              //     child: Column(
+              //       // mainAxisAlignment: MainAxisAlignment.center,
+              //       children: [
+              //         Opacity(
+              //           opacity: 0.8,
+              //           child: Padding(
+              //             padding: const EdgeInsets.all(8.0),
+              //             child: Image.asset(
+              //               "assets/images/lwazin.png",
+              //               width: 80,
+              //               height: 80,
+              //             ),
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
@@ -34,9 +46,74 @@ class _WrapperPageState extends State<WrapperPage> {
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(5),
                       child: Container(
-                        color: Colors.white,
-                        width: 400,
-                        height: 200,
+                        color: Colors.grey[300],
+                        width: 300,
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Image.network(
+                              "https://upload.wikimedia.org/wikipedia/en/d/de/Cohiba_cigar_logo.png",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Container(
+                        color: Colors.grey[300],
+                        width: 300,
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Image.network(
+                              "https://upload.wikimedia.org/wikipedia/en/d/da/H_upmann_cigars_logo.png",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Container(
+                        color: Colors.grey[300],
+                        width: 300,
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Image.network(
+                              "https://upload.wikimedia.org/wikipedia/en/3/32/Montecristo_cigar_logo.png",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Container(
+                        color: Colors.grey[300],
+                        width: 300,
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Image.network(
+                              "https://upload.wikimedia.org/wikipedia/en/4/42/Rj_logo.png",
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
